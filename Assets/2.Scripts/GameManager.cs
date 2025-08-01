03f08d038ce4c16a14d85914c8d0fb9fe6f0c265
aljae <minjae8737@gmail.com>
2025-08-01 10:59:26 +0900

    Feat: 게임매니저 생성

---
+++ Assets/2.Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public Player player;
+
+    const string Gold = "Gold";
+    public long gold;
+    long maxGold = 9999999999; // 9,999,999,999
+
+    void Awake()
+    {
+        instance = this;
+        Application.targetFrameRate = 60;
+        Init();
+    }
+
+    void Init()
+    {
+        gold = 0L;
+
+        if (PlayerPrefs.HasKey(Gold))
+        {
+            string goldStr = PlayerPrefs.GetString(Gold);
+            gold = long.TryParse(goldStr, out long result) ? result : 0L;
+        }
+    }
+
+    void SaveGold()
+    {
+        PlayerPrefs.SetString(Gold, gold.ToString());
+    }
+
+    void RestPlayer()
+    {
+        // player.Rest();
+    }
+
+
+
+}

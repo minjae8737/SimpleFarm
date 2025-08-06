07e7c6f5516725644c15dabe5c3e30030fd7491e
aljae <minjae8737@gmail.com>
2025-08-06 17:37:45 +0900

    Feat: IslandManager 로직 작업

--- Assets/2.Scripts/FarmData.cs
+++ Assets/2.Scripts/FarmData.cs
@@ -2,17 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class FarmData
 {
     public string name;
-    [Header("농장 레벨")]
+    [Header("최대 농장 레벨")]
     public int maxFarmLevel;
-    [Header("자동 생산 확률 레벨")]
+    [Header("농장 기본 골드")]
+    public int basicFarmGold;
+    [Header("레벨별 농장 골드 증가율")]
+    public float farmGoldPer;
+
+    [Space(10)]
+    [Header("최대 자동 생산 확률 레벨")]
     public int maxAutoProduceCanceLevel;
-    [Header("기본 자동 생산 확률")]
+    [Header("기본 자동 생산 확률(%)")]
     public float basicAutoProduceCance;
-    [Header("생산 시간 레벨")]
+    [Header("레벨당 생산 확률 증가(%)")]
+    public float autoProduceChancePer;
+    [Header("자동 생산 확률 기본 골드")]
+    public int basicAutoProduceCanceGold;
+    [Header("레벨별 자동 생산 확률 골드 증가율")]
+    public float autoProduceCanceGoldPer;
+
+
+    [Space(10)]
+    [Header("최대 생산 시간 레벨")]
     public int maxProduceCooldownLevel;
     [Header("기본 생산 시간")]
     public float basicProduceCooldown;
+    [Header("레벨당 생산 시간 감소(sec)")]
+    public float produceCooldownPer;
+    [Header("생산 시간 감소 기본 골드")]
+    public int basicProduceCooldownGold;
+    [Header("레벨별 생산 시간 감소 골드 증가율")]
+    public float produceCooldownGoldPer;
+
 }
--- Assets/2.Scripts/IslandManager.cs
+++ Assets/2.Scripts/IslandManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,15 +6,62 @@ using UnityEngine;
 public class IslandManager : MonoBehaviour
 {
 
-    public QuestData[] datas;
+    public IslandData[] datas;
+    public Dictionary<string, int[]> islandLevelDic;
+    string IslandKey = "Island_";
 
-    void Start()
+    void Init()
     {
+        // Island level 초기화
+        for (int i = 0; i < datas.Length; i++)
+        {
+            string key = IslandKey + (i + 1); // key = Island_'n'
+            int[] levelArr = new int[3];  // 각 level을 저장할 배열
 
+            int farmLevel = PlayerPrefs.HasKey(key + "_FarmLevel") ? PlayerPrefs.GetInt(key + "_FarmLevel") : 1; // key = Island_'n'_FarmLevel
+            int autoLevel = PlayerPrefs.HasKey(key + "_AutoLevel") ? PlayerPrefs.GetInt(key + "_AutoLevel") : 1; // key = Island_'n'_AutoLevel
+            int cooldownLevel = PlayerPrefs.HasKey(key + "_CoolDownLevel") ? PlayerPrefs.GetInt(key + "_CoolDownLevel") : 1; // key = Island_'n'_CoolDownLevel
+
+            islandLevelDic.Add(key, levelArr);
+        }
     }
 
-    void Update()
+    public int GetUpgradeFarmGold(int islandIndex)
     {
+        FarmData farmData = datas[islandIndex].farmData;
+        int level = islandLevelDic[IslandKey + (islandIndex + 1)][0];
+
+        return Mathf.FloorToInt(farmData.basicFarmGold * Mathf.Pow(farmData.farmGoldPer, level - 1)); // level 수정
+    }
+
+    public int GetUpgradeAutoProduceCanceGold(int islandIndex)
+    {
+        FarmData farmData = datas[islandIndex].farmData;
+        int level = islandLevelDic[IslandKey + (islandIndex + 1)][1];
+
+        return Mathf.FloorToInt(farmData.basicAutoProduceCanceGold * Mathf.Pow(farmData.autoProduceCanceGoldPer, level - 1)); // level 수정
+    }
+
+    public int GetUpgradeProduceCooldownGold(int islandIndex)
+    {
+        FarmData farmData = datas[islandIndex].farmData;
+        int level = islandLevelDic[IslandKey + (islandIndex + 1)][2];
+
+        return Mathf.FloorToInt(farmData.basicProduceCooldownGold * Mathf.Pow(farmData.produceCooldownGoldPer, level - 1)); // level 수정
+    }
+
+    public void SaveData()
+    {
+        // levelData
+        for (int i = 0; i < datas.Length; i++)
+        {
+            string key = IslandKey + (i + 1); // key = Island_'n'
+            int[] levelArr = islandLevelDic[key];  // 각 level을 저장한 배열
+
+            PlayerPrefs.SetInt(key + "_FarmLevel", levelArr[0]); // key = Island_'n'_FarmLevel
+            PlayerPrefs.SetInt(key + "_AutoLevel", levelArr[1]); // key = Island_'n'_AutoLevel
+            PlayerPrefs.SetInt(key + "_CoolDownLevel", levelArr[2]); // key = Island_'n'_CoolDownLevel
+        }
 
     }
 }

a802445bc06968c31fdeb9e8a806346797577e10
aljae <minjae8737@gmail.com>
2025-09-08 17:08:14 +0900

    Feat: TilemapBaker 추가, Marker 로직 및 스프라이트 변경, 타일맵 추가

--- Assets/2.Scripts/Marker.cs
+++ Assets/2.Scripts/Marker.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
@@ -6,8 +7,21 @@ using UnityEngine;
 public class Marker : MonoBehaviour
 {
     public GameObject marker;
-
+    
+    public Transform marker_bl;
+    public Transform marker_br;
+    public Transform marker_tr;
+    public Transform marker_tl;
     Tween scaleTween;
+
+
+    private void Awake()
+    {
+        marker_bl = marker.transform.GetChild(0);
+        marker_br = marker.transform.GetChild(1);
+        marker_tr = marker.transform.GetChild(2);
+        marker_tl = marker.transform.GetChild(3);
+    }
 
     void OnEnable()
     {
@@ -31,9 +45,13 @@ public class Marker : MonoBehaviour
 
     void OnSacleEffect()
     {
-        scaleTween = marker?.transform.DOScale(Vector3.one * 1.02f, 0.5f)
-        .SetEase(Ease.Linear)
-        .SetLoops(-1, LoopType.Yoyo);
+        marker_bl?.DOMove(marker_bl.position + (Vector3.left + Vector3.down) * 0.1f,0.5f).SetLoops(-1, LoopType.Yoyo); 
+        marker_br?.DOMove(marker_br.position + (Vector3.right + Vector3.down) * 0.1f,0.5f).SetLoops(-1, LoopType.Yoyo);
+        marker_tr?.DOMove(marker_tr.position + (Vector3.right + Vector3.up) * 0.1f,0.5f).SetLoops(-1, LoopType.Yoyo);
+        marker_tl?.DOMove(marker_tl.position + (Vector3.left + Vector3.up) * 0.1f,0.5f).SetLoops(-1, LoopType.Yoyo);
+        
+        // scaleTween = marker?.transform.DOScale(Vector3.one * 1.05f, 0.5f)
+        // .SetLoops(-1, LoopType.Yoyo);
     }
 
 }

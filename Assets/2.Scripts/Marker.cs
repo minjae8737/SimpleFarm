3948cd14e7c2d6be37e984a329880d6a186beaac
aljae <minjae8737@gmail.com>
2025-10-21 17:46:52 +0900

    Fix: Maker DoTween 메모리 누수 방지 로직 추가

--- Assets/2.Scripts/Marker.cs
+++ Assets/2.Scripts/Marker.cs
@@ -2,17 +2,27 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Marker : MonoBehaviour
 {
     public GameObject marker;
-    
-    Transform marker_bl;
-    Transform marker_br;
-    Transform marker_tr;
-    Transform marker_tl;
 
+    private Transform marker_bl;
+    private Transform marker_br;
+    private Transform marker_tr;
+    private Transform marker_tl;
+    
+    private Vector3 originalPos_bl;
+    private Vector3 originalPos_br;
+    private Vector3 originalPos_tr;
+    private Vector3 originalPos_tl;
+    
+    private Tween tween_bl;
+    private Tween tween_br;
+    private Tween tween_tr;
+    private Tween tween_tl;
 
     private void Awake()
     {
@@ -20,11 +30,22 @@ public class Marker : MonoBehaviour
         marker_br = marker.transform.GetChild(1);
         marker_tr = marker.transform.GetChild(2);
         marker_tl = marker.transform.GetChild(3);
+        
+        originalPos_bl = marker_bl.localPosition;
+        originalPos_br = marker_br.localPosition;
+        originalPos_tr = marker_tr.localPosition;
+        originalPos_tl = marker_tl.localPosition;
     }
 
-    void OnEnable()
+    private void OnEnable()
     {
+        ResetPositions();
         OnSacleEffect();
+    }
+
+    private void OnDisable()
+    {
+        KillAllTweens();
     }
 
     public void OnMarker()
@@ -36,13 +57,35 @@ public class Marker : MonoBehaviour
     {
         marker.SetActive(false);
     }
-
-    void OnSacleEffect()
+    
+    private void ResetPositions()
     {
-        marker_bl?.DOMove(marker_bl.position + (Vector3.left + Vector3.down) * 0.1f,0.5f).SetLoops(-1, LoopType.Yoyo); 
-        marker_br?.DOMove(marker_br.position + (Vector3.right + Vector3.down) * 0.1f,0.5f).SetLoops(-1, LoopType.Yoyo);
-        marker_tr?.DOMove(marker_tr.position + (Vector3.right + Vector3.up) * 0.1f,0.5f).SetLoops(-1, LoopType.Yoyo);
-        marker_tl?.DOMove(marker_tl.position + (Vector3.left + Vector3.up) * 0.1f,0.5f).SetLoops(-1, LoopType.Yoyo);
+        if (marker_bl != null) marker_bl.localPosition = originalPos_bl;
+        if (marker_br != null) marker_br.localPosition = originalPos_br;
+        if (marker_tr != null) marker_tr.localPosition = originalPos_tr;
+        if (marker_tl != null) marker_tl.localPosition = originalPos_tl;
     }
 
+    private void OnSacleEffect()
+    {
+        KillAllTweens();
+        
+        tween_bl = marker_bl?.DOMove(marker_bl.localPosition + (Vector3.left + Vector3.down) * 0.1f, 0.5f).SetLoops(-1, LoopType.Yoyo);
+        tween_br = marker_br?.DOMove(marker_br.localPosition + (Vector3.right + Vector3.down) * 0.1f, 0.5f).SetLoops(-1, LoopType.Yoyo);
+        tween_tr = marker_tr?.DOMove(marker_tr.localPosition + (Vector3.right + Vector3.up) * 0.1f, 0.5f).SetLoops(-1, LoopType.Yoyo);
+        tween_tl = marker_tl?.DOMove(marker_tl.localPosition + (Vector3.left + Vector3.up) * 0.1f, 0.5f).SetLoops(-1, LoopType.Yoyo);
+    }
+    
+    private void KillAllTweens()
+    {
+        tween_bl?.Kill();
+        tween_br?.Kill();
+        tween_tr?.Kill();
+        tween_tl?.Kill();
+        
+        tween_bl = null;
+        tween_br = null;
+        tween_tr = null;
+        tween_tl = null;
+    }
 }
